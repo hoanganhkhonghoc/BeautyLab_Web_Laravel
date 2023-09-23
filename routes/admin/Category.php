3919a5809026487a391062c1bf83e35da6cafd31
hoanganhkhonghoc <hoanganhkhonghoc@gmail.com>
2023-09-23 22:57:22 +0700

    project2

--- routes/admin/Category.php
+++ routes/admin/Category.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\admin\AdminCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("/admin/category")->middleware("checkQuyenSanPham")->group(function(){
-    Route::get("/list",             [AdminCategoryController::class, "list"])       ->name("listCategory");
-    Route::get("/addView",          [AdminCategoryController::class, "add"])        ->name("addCategory");
-    Route::post("/xl_add",          [AdminCategoryController::class, "xl_add"]);
-    Route::get("/xl_delete/{id}",   [AdminCategoryController::class, "xl_delete"]);
-    Route::get("/editView/{id}",    [AdminCategoryController::class, "edit"]);
-    Route::post("/xl_edit/{id}",    [AdminCategoryController::class, "xl_edit"]);
+    Route::get  ("/list",             [AdminCategoryController::class, "list"])       ->name("listCategory");
+    Route::get  ("/addView",          [AdminCategoryController::class, "add"])        ->name("addCategory");
+    Route::post ("/xl_add",           [AdminCategoryController::class, "xl_add"]);
+    Route::get  ("/xl_delete/{id}",   [AdminCategoryController::class, "xl_delete"]);
+    Route::get  ("/editView/{id}",    [AdminCategoryController::class, "edit"]);
+    Route::post ("/xl_edit/{id}",     [AdminCategoryController::class, "xl_edit"]);
 })
 ?>
